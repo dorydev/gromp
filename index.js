06ce23aea94fa6d16414dcf25d6beaa4149864e3
dory <95535428+dorydev@users.noreply.github.com>
2025-04-28 10:53:48 +0200

    update command handler

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const { Client, GatewayIntentBits, Events, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Events, Collection, REST, Routes } = require('discord.js');
 const mongoose = require('mongoose');
 const moment = require('moment');
 const config = require('./config.json');
-const fs = require("fs");
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds]
@@ -12,9 +12,60 @@ const client = new Client({
 
 client.config = config;
 
+
+
 //command handler
 
 client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+//fin command handler
+
+
+const commandsArray = client.commands.map(cmd => cmd.data.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commandsArray.length} application (/) commands.`);
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commandsArray },
+        );
+
+        console.log(`Successfully reloaded application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
 
 
 (async () => {
