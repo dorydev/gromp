5ebd0bece0e47c37b9be37f595478662a9bd7caf
dory <95535428+dorydev@users.noreply.github.com>
2025-05-20 09:21:48 +0200

    refactor command handler to support subdirectories for commands

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const client = new Client({
 
 client.config = config;
 
-
-
 //command handler
 
 client.commands = new Collection();
@@ -21,14 +19,29 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
+
+for (const folder of fs.readdirSync(commandsPath)) {
+	if (fs.statSync(path.join(commandsPath, folder)).isDirectory()) {
+		const subCommandFiles = fs.readdirSync(path.join(commandsPath, folder)).filter(file => file.endsWith('.js'));
+		for (const file of subCommandFiles) {
+			const filePath = path.join(commandsPath, folder, file);
+			const command = require(filePath);
+			if ('data' in command && 'execute' in command) {
+				client.commands.set(command.data.name, command);
+			} else {
+				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			}
+		}
+	}
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 const eventsPath = path.join(__dirname, 'events');
@@ -66,6 +79,12 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
     }
 })();
 
+
+const autoReset = require('./events/autoReset')(client);
+const logSender = require('./events/logSender');
+logSender(client);
+
+const giveUserXp = require('./events/giveUserXp');
 
 
 (async () => {
