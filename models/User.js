3073cb3790861ab465f1c348c86d7d8d93d7892c
dory <95535428+dorydev@users.noreply.github.com>
2025-05-20 09:23:16 +0200

    add User model for managing user data and experience points

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+	userId: String,
+	guildId: String,
+	xp: {
+		type: Number,
+		default: 0
+	},
+	messages: {
+		type: Number,
+		default: 0
+	},
+	lastDaily: {
+		type: Date,
+		default: null
+	}
+});
+
+module.exports = mongoose.model('User', userSchema);
